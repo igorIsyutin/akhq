3f74ef245812c3bfe916dce18f2d36075821d9cb
Igor Isyutin <igor.isyutin@gmail.com>
2020-09-09 00:34:44 +0300

    update configs and topic cnotroller

--- src/main/java/org/akhq/controllers/TopicController.java
+++ src/main/java/org/akhq/controllers/TopicController.java
@@ -200,7 +200,7 @@ public class TopicController extends AbstractController {
     @Get("api/{cluster}/topic/{topicName}/groups")
     @Operation(tags = {"topic"}, summary = "List all consumer groups from a topic")
     public List<ConsumerGroup> groups(String cluster, String topicName) throws ExecutionException, InterruptedException {
-        return this.topicRepository.findByName(cluster, topicName, skipConsumerGroups).getConsumerGroups();
+        return this.topicRepository.findByName(cluster, topicName, false).getConsumerGroups();
     }
 
     @Get("api/{cluster}/topic/{topicName}/configs")
